e67e71cd65c2a64fd4f1fea8d46aa7e035b1f0d9
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-12-18 18:47:17 -0500

    deleted Fragment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component, Fragment } from 'react';
+import { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
